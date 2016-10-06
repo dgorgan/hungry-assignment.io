b8b2020913c255220821f6d4a0a5d05fecbf91bd
Dumitru Gorgan <dgorgan3@gmail.com>
2016-10-06 17:06:11 -0400

    added functionality to close model by clicking outside of it

--- js/main.js
+++ js/main.js
@@ -23,10 +23,23 @@ $(document).ready(function() {
     //     alert('#FF00FF is Red, No Green, Blue!');
     // })
 
-    $('.order-now').on('click',function(){
-      $('.jumbotron').toggleClass('background-modal-opacity')
-      $('.calendar-picker').toggleClass('background-modal-opacity')
-      $('.hungry-bag-icn').toggleClass('background-modal-opacity')
-      $('.form-signup').show().toggleClass('open')
+    $('.order-now').on('click', function(e) {
+        $('body').data('form-focus', true);
+        e.stopPropagation();
+        $('.jumbotron').addClass('background-modal-opacity');
+        $('.calendar-picker').addClass('background-modal-opacity');
+        $('.hungry-bag-icn').addClass('background-modal-opacity');
+        $('.form-signup').addClass('open').fadeIn('fast');
     })
-  });
+
+    $('body').on("click", 'div:not(.form-signup.open)', function(e){
+      e.stopPropagation()
+      if ( $('body').data('form-focus') ) {
+        $('body').data('form-focus', false);
+        $('.form-signup').removeClass('open').hide();
+        $('.jumbotron').removeClass('background-modal-opacity');
+        $('.calendar-picker').removeClass('background-modal-opacity');
+        $('.hungry-bag-icn').removeClass('background-modal-opacity');
+      }
+    })
+});
