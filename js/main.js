9cea6bcd3967f81d64ee13730c2f444077bcc5ae
Dumitru Gorgan <dgorgan3@gmail.com>
2016-10-06 15:34:37 -0400

    added sign-up-form modal popup on order now button click

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
-$( document ).ready(function() {
-  $('.nav-tabs>li').on('click',function() {
-     $('div.calendar-picker > ul > li').each(function(index, value){
+$(document).ready(function() {
+    $('.nav-tabs>li').on('click', function() {
+        $('div.calendar-picker > ul > li').each(function(index, value) {
 
-      // tab.removeClass('selected-tab');
-      console.log( index + ": " + value.outerHTML );
+            // tab.removeClass('selected-tab');
+            console.log(index + ": " + value.outerHTML);
 
-      var outterHTMLZ = value.outterHTML;
+            var outterHTMLZ = value.outterHTML;
 
 
-      $(outterHTMLZ).removeClass('selected-tab');
+            $(outterHTMLZ).removeClass('selected-tab');
 
+        })
+
+        // $.each('div.calendar-picker > ul > li', function(val) {
+        //   $(val).removeClass('selected-tab');
+        // });
+
+        $(this).toggleClass('selected-tab');
+    });
+
+    // $('body > div.jumbotron > div > div.col-md-5.info-right > button').on('click', function() {
+    //     alert('#FF00FF is Red, No Green, Blue!');
+    // })
+
+    $('.order-now').on('click',function(){
+      $('.jumbotron').toggleClass('background-modal-opacity')
+      $('.calendar-picker').toggleClass('background-modal-opacity')
+      $('.hungry-bag-icn').toggleClass('background-modal-opacity')
+      $('.form-signup').show().toggleClass('open')
     })
-
-    // $.each('div.calendar-picker > ul > li', function(val) {
-    //   $(val).removeClass('selected-tab');
-    // });
-
-    $(this).toggleClass('selected-tab');
   });
-
-  $('body > div.jumbotron > div > div.col-md-5.info-right > button').on('click', function() {
-    alert('#FF00FF is Red, No Green, Blue!');
-  })
-});
